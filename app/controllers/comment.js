7c99fefea34f669804701ccf8262e4b63769ec83
Johan Poirier <johan.poirier@gmail.com>
2014-07-09 17:34:28 +0200

    added comment controller

--- app/controllers/comment.js
+++ app/controllers/comment.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    needs: ['application'],
+
+    // REGRESSION hack
+    author: Ember.computed.alias('model.author'),
+    date: Ember.computed.alias('model.date'),
+    text: Ember.computed.alias('model.text'),
+
+    isLogged: function() {
+        console.debug("is logged : ", this.get("controllers.application").get("isLogged"));
+    }.property(),
+
+    dateFormat: function() {
+        return Ember.I18n.translations["dateFormat"];
+    }.property(),
+
+    actions: {
+        "delete": function() {
+            var comment = this.get('model');
+            comment.deleteRecord();
+            comment.save();
+        }
+    }
 });
